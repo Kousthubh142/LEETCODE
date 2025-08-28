40aa51a724e614acc9ba06865a5d099d73a868d5
Kousthubh N <133272040+Kousthubh142@users.noreply.github.com>
2025-08-28 18:57:58 +0530

    Time: 0 ms (100%), Space: 10.8 MB (92.78%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -6,24 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        dfs(root, ans);
+        dfs(root,ans);
         return ans;
     }
-
 private:
     void dfs(TreeNode* root, vector<int>& ans) {
-        if (!root) return;
-        ans.push_back(root->val);      // Visit root
-        dfs(root->left, ans);          // Traverse left
-        dfs(root->right, ans);         // Traverse right
+        if(!root) return;
+
+        ans.push_back(root->val);
+        dfs(root->left,ans);
+        dfs(root->right,ans);
     }
 };
