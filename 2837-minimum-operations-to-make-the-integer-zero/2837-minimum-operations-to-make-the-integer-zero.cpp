08909febee3fb0713cf448e01aa90a7e88e9935c
Kousthubh N <133272040+Kousthubh142@users.noreply.github.com>
2025-09-05 11:45:08 +0530

    Time: 0 ms (100%), Space: 8.1 MB (85.95%) - LeetHub

---
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int makeTheIntegerZero(int num1, int num2) {
+        
+
+        for(int i=0;i<=60;i++) {
+            long long target=num1 - i*1LL*num2;
+
+            if(target<=0) return -1;
+
+            int bits=__builtin_popcountll(target);
+
+            if(bits<=i && target>=i) return i;
+        }
+        return -1;
+    }
+};
