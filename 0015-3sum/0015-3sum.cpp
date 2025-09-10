e04da9dcb8f3eb42ecfd102622b5bc2810b6b53d
Kousthubh N <133272040+Kousthubh142@users.noreply.github.com>
2025-09-10 18:35:06 +0530

    Time: 44 ms (79.19%), Space: 29 MB (71.91%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,20 +7,18 @@ public:
 
         for(int i=0;i<nums.size();i++) {
             if(i>0 && nums[i]==nums[i-1]) continue;
-
             int j=i+1;
             int k=nums.size()-1;
 
             while(j<k) {
                 int sum=nums[i]+nums[j]+nums[k];
-
-                if(sum<0) {
+                if(sum<0){
                     j++;
                 }
                 else if(sum>0) {
                     k--;
                 }
-                else{
+                else {
                     ans.push_back({nums[i],nums[j],nums[k]});
                     j++;
                     k--;
