2481dcb2147743e2655606810bbca6a87fc023ea
Kousthubh N <133272040+Kousthubh142@users.noreply.github.com>
2025-09-10 18:43:17 +0530

    Time: 0 ms (100%), Space: 23.7 MB (80.14%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // if(!intervals.size()) return {};
+        // sort(intervals.begin(),intervals.end());
+
+        // vector<vector<int>> merged;
+
+        // for(auto& interval:intervals) {
+        //     if(merged.empty() || merged.back()[1]<interval[0]) 
+        //         merged.push_back(interval);
+        //     else {
+        //         merged.back()[1]=max(merged.back()[1],interval[1]);
+        //     }
+        // }
+        // return merged;
+
+        if(!intervals.size()) return {};
+        vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end());
 
-        vector<vector<int>> merged;
 
         for(auto& interval:intervals) {
-            if(merged.empty() || merged.back()[1]<interval[0]) 
-                merged.push_back(interval);
+            if(ans.empty() || ans.back()[1]<interval[0]) {
+                ans.push_back(interval);
+            }
             else {
-                merged.back()[1]=max(merged.back()[1],interval[1]);
+                ans.back()[1]=max(ans.back()[1],interval[1]);
             }
         }
-        return merged;
+        return ans;
     }
 };
